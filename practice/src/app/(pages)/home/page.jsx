b620c60d861cfe3bedf18ba76037beedb115f393
tanvirtbf <tanver018765@gmail.com>
2024-09-23 10:21:30 +0600

    Link Component vs anchor tag

--- practice/src/app/(pages)/home/page.jsx
+++ practice/src/app/(pages)/home/page.jsx
@@ -1,11 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
-const page = () => {
+const HomePage = () => {
   return (
-    <div>
-      Page
-    </div>
+    <>
+      <div className="flex gap-2 border border-green-600 m-2 justify-between p-4">
+        <Link href="/home">Home</Link>
+        <Link href="/category/1/hello/2/hii/3">Category</Link>
+        <Link href="/gallery">Gallery</Link>
+      </div>
+    </>
   )
 }
 
-export default page
+export default HomePage
