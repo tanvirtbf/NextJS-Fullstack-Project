e252316afef34de926a067b0798bd7901e8294de
tanvirtbf <tanver018765@gmail.com>
2024-09-25 12:09:40 +0600

    form style change

--- practice/src/app/api/register/route.js
+++ practice/src/app/api/register/route.js
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request, response){
-  return NextResponse.json({message:'Get Request Fired'})
+export async function GET(request, response) {
+  return NextResponse.json({
+    message: "Get Request Fired",
+    data: { firstName: "Tanvir", lastName: "Ahmed", age: 25 },
+  });
 }
--- practice/src/components/ReactForm.jsx
+++ practice/src/components/ReactForm.jsx
@@ -5,18 +5,17 @@ import { useState } from "react";
 
 const ReactForm = () => {
   const {register,handleSubmit, formState:{errors}} = useForm()
-
   const [message,setMessage] = useState('')
 
-  async function onSubmit(form){
-    await axios.get("/api/register").then(res => {
+  const onSubmit = async (form)=> {
+    await axios.get('/api/register').then((res)=> {
       setMessage(res.data.message)
     })
   }
 
   return (
-    <div className="max-w-xl mx-auto mt-20">
-      <div>
+    <div className="max-w-xl mx-auto mt-20 px-4">
+      <div className="w-full bg-blue-100 px-6 py-12 rounded-lg flex justify-center">
         <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
           <div className="flex items-center"> 
             <label htmlFor="" className="w-24">Email Id</label>
@@ -28,12 +27,12 @@ const ReactForm = () => {
             <input className="px-2 py-2 outline-none border border-gray-400 rounded-md" type="text" {...register('password', {required:true, minLength:5})} />
             {errors.password?.type==='minLength' && <p className="text-red-400">Please Enter minimum 5 Length of character</p>}
           </div>
-          <div>
-            <button className="px-5 py-2 bg-orange-800 text-white font-bold" type="submit">Submit</button>
+          <div className="w-full flex justify-center">
+            <button className="w-full rounded-lg px-5 py-2 bg-orange-800 text-white font-bold" type="submit">Submit</button>
           </div>
         </form>
         <div>
-          <p>{message}</p>
+          {message} 
         </div>
       </div>
     </div>
