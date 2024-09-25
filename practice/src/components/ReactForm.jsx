9c24c1e6fd174d0e516f7939e93f6d3d80720a08
tanvirtbf <tanver018765@gmail.com>
2024-09-25 11:46:56 +0600

    axios get method test with form

--- practice/src/components/ReactForm.jsx
+++ practice/src/components/ReactForm.jsx
@@ -1,10 +1,17 @@
 "use client";
+import axios from "axios";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const ReactForm = () => {
   const {register,handleSubmit, formState:{errors}} = useForm()
-  function onSubmit(form){
-    console.log(form)
+
+  const [message,setMessage] = useState('')
+
+  async function onSubmit(form){
+    await axios.get("/api/register").then(res => {
+      setMessage(res.data.message)
+    })
   }
 
   return (
@@ -13,8 +20,8 @@ const ReactForm = () => {
         <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
           <div className="flex items-center"> 
             <label htmlFor="" className="w-24">Email Id</label>
-            <input className="px-2 py-2 outline-none border border-gray-400 rounded-md" type="text" {...register("email", {required:true, maxLength:15})}  />
-            {errors.email?.type==='maxLength' && <p className="text-red-400">Please Enter under 16 length of character!</p>}
+            <input className="px-2 py-2 outline-none border border-gray-400 rounded-md" type="text" {...register("email", {required:true, maxLength:45})}  />
+            {errors.email?.type==='maxLength' && <p className="text-red-400">Please Enter under 45 length of character!</p>}
           </div>
           <div className="flex items-center">
             <label htmlFor="" className="w-24">Password</label>
@@ -25,6 +32,9 @@ const ReactForm = () => {
             <button className="px-5 py-2 bg-orange-800 text-white font-bold" type="submit">Submit</button>
           </div>
         </form>
+        <div>
+          <p>{message}</p>
+        </div>
       </div>
     </div>
   );
