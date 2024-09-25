4f3976e334a21c75ee86fc91b3fc46a8a1351f9b
tanvirtbf <tanver018765@gmail.com>
2024-09-25 11:21:33 +0600

    style change

--- practice/src/components/ReactForm.jsx
+++ practice/src/components/ReactForm.jsx
@@ -6,23 +6,23 @@ const ReactForm = () => {
   function onSubmit(form){
     console.log(form)
   }
-  
+
   return (
     <div className="max-w-xl mx-auto mt-20">
       <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div>
-            <label htmlFor="">Email Id</label>
-            <input className="px-2 py-2 outline-none border border-gray-400" type="text" {...register("email", {required:true, maxLength:15})}  />
+        <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex items-center"> 
+            <label htmlFor="" className="w-24">Email Id</label>
+            <input className="px-2 py-2 outline-none border border-gray-400 rounded-md" type="text" {...register("email", {required:true, maxLength:15})}  />
             {errors.email?.type==='maxLength' && <p className="text-red-400">Please Enter under 16 length of character!</p>}
           </div>
-          <div>
-            <label htmlFor="">Password</label>
-            <input className="px-2 py-2 outline-none border border-gray-400" type="text" {...register('password', {required:true, minLength:5})} />
+          <div className="flex items-center">
+            <label htmlFor="" className="w-24">Password</label>
+            <input className="px-2 py-2 outline-none border border-gray-400 rounded-md" type="text" {...register('password', {required:true, minLength:5})} />
             {errors.password?.type==='minLength' && <p className="text-red-400">Please Enter minimum 5 Length of character</p>}
           </div>
           <div>
-            <button className="px-5 py-2 bg-orange-800 text-white font-bold mt-5" type="submit">Submit</button>
+            <button className="px-5 py-2 bg-orange-800 text-white font-bold" type="submit">Submit</button>
           </div>
         </form>
       </div>
